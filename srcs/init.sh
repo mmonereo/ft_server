838b35cb7a08342193287222ad7c838ecc709f35
Mauro Monereo <mauromonereo@MacBook-Pro-de-Mauro.local>
2021-03-24 09:53:18 +0100

    permisions

--- srcs/init.sh
+++ srcs/init.sh
@@ -45,8 +45,3 @@ service nginx start
  # docker run --name tapa -d -p 80:80 -p 443:443 demo
  # docker exec -it  tapa bash
 
-# service mysql start
-# echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
-# echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" | mysql -u root --skip-password
-# echo "FLUSH PRIVILEGES;" | mysql -u root --skip-password
-# echo "update mysql.user set plugin = 'mysql_native_password' where user='root';" | mysql -u root
